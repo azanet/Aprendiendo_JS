4080b9c44a546a84b5115294645fe08fe15413af
azanet <azan3t@gmail.com>
2021-04-11 12:50:47 +0100

    Indice para acceder, creacion de objetos con JSON

---
+++ 10_WorkWithJSON_DEMO/WorkWithJSON.js
@@ -0,0 +1,51 @@
+//JSON Javascript Object Notation
+//Se utilizan para crear OBJETOS
+// y trabajan 'similar' a los Diccionarios de Java (Clave:Valor)
+
+
+
+// 1)
+//////////////////////////////////////////////////////////////////////
+//Al definir un JSON, las "KEY" podemos definirlas con o sin comillas.
+
+let jKeySinComillas = {
+    a:"1111",
+    b:"2222",
+    c:"33333",
+    d:"4444"
+};
+
+
+
+let jKeyConComillas = {"e":"5555","f":"6666","g":"7777","h":"8888"};
+
+
+//Listando los dos Arrays de OBJETOS
+for (let keyS in jKeySinComillas){
+
+    document.write(jKeySinComillas[keyS]+"<br>");
+}
+
+document.write("</p>");
+for (let key in jKeyConComillas){
+
+    document.write(jKeyConComillas[key]+"<br>");
+}
+document.write("</p>");
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////
+// 2)
+//////////////////////////////////////////////////////////////////////
+
+//Creando un OBJETO, con atributos y funciones que utilizan sus atributos
+let objPersona= {
+
+
+
+};
+
